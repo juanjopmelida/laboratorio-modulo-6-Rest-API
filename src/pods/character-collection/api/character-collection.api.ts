3fc8cc9e5b35f7761f5d3c652da9cd53244894a9
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-10-25 08:26:53 +0100

    RC

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,16 +1,9 @@
+import Axios from 'axios'
 import { Character } from '../../character/api/character.api-model';
 
 const characterListUrl = '/api/character/';
 
-export const getCharacterCollection = async (): Promise<
-  Character[]
-> => {
-  const response = await fetch(
-    `${process.env.API_CHARACTERS_URL}${characterListUrl}`
-  );
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw Error(response.statusText);
-  }
+export const getCharacterCollection = async (): Promise<Character[]> => {
+  const {data} = await Axios.get(`${process.env.API_CHARACTERS_URL}${characterListUrl}`);
+  return data.results;
 };
